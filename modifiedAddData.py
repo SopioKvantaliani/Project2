0e4f4c74ab5db2699ea7b13372b1a38318c393dc
sopiokvantaliani <skvantaliani@gmail.com>
2023-12-11 21:02:38 -0500

    modified code to add db in postgreSQL and csv file

--- modifiedAddData.py
+++ modifiedAddData.py
@@ -1,25 +1,10 @@
 import csv
 import os
 import psycopg2
+from psycopg2 import sql
 
 path = '/Users/sopiokvantaliani/PythonApplications/Project2'
 file_path = os.path.join(path, "data.csv")
-
-# Check if the file exists, create it if not
-if not os.path.isfile(file_path):
-    with open(file_path, "w", newline='', encoding='utf-8') as file:
-        fieldnames = ['full_name', 'first_name', 'last_name', 'title',
-                      'organization_name', 'email', 'linkedin', 'twitter']
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-
-        # Write header
-        writer.writeheader()
-
-# Get input from the user
-new_data = {}
-print("Enter the following details:")
-for field in ['full_name', 'first_name', 'last_name', 'title', 'organization_name', 'email', 'linkedin', 'twitter']:
-    new_data[field] = input(f"{field.capitalize()}: ")
 
 # Establish a connection to the PostgreSQL database
 con = psycopg2.connect(
@@ -31,6 +16,58 @@ con = psycopg2.connect(
 )
 
 cursor = con.cursor()
+
+# Create the 'clientDb' table if it doesn't exist
+create_table_query = """
+    CREATE TABLE IF NOT EXISTS clientDb (
+        full_name TEXT,
+        first_name TEXT,
+        last_name TEXT,
+        title TEXT,
+        organization_name TEXT,
+        email TEXT,
+        linkedin TEXT,
+        twitter TEXT
+    )
+"""
+cursor.execute(create_table_query)
+
+# Commit the changes to ensure the table creation is applied
+con.commit()
+
+
+# Define fieldnames variable
+fieldnames = ['full_name', 'first_name', 'last_name', 'title',
+              'organization_name', 'email', 'linkedin', 'twitter']
+
+# Check if the file exists, create it if not
+if not os.path.isfile(file_path):
+    with open(file_path, "w", newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+        # Write header
+        writer.writeheader()
+
+# Get input from the user
+new_data = {}
+print("Enter the following details:")
+for field in ['full_name', 'first_name', 'last_name', 'title', 'organization_name', 'email', 'linkedin', 'twitter']:
+    new_data[field] = input(f"{field.capitalize()}: ")
+
+with open(file_path, "r", newline='', encoding='utf-8') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    if any(row['email'] == new_data['email'] for row in csv_reader):
+        print("Error: Email address already exists in the CSV file.")
+    else:
+        # Reset the file position to the beginning before writing
+        csv_file.seek(0)
+        # Insert the new data into the CSV file
+        with open(file_path, "a", newline='', encoding='utf-8') as csv_file:
+            csv_writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+            if csv_file.tell() == 0:
+                # Write header only if the file is empty
+                csv_writer.writeheader()
+            csv_writer.writerow(new_data)
 
 # Check if the email already exists in the 'clientDb' table
 cursor.execute("SELECT email FROM clientDb WHERE email = %s",
